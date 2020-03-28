18482f474e0d5073988b7a0208d8e682e50ee5a8
Drew Anderson <drew@onlycentral.com>
2020-03-28 18:36:38 +0100

    demo url

--- serve.bash
+++ serve.bash
@@ -5,6 +5,8 @@ if [ ! -f ./.git/config ]; then
     exit 1;
 fi
 
+echo "Browse to http://127.0.0.1:8080/ to see the served website"
+
 docker run --rm -it \
   --volume="$PWD:/srv/jekyll" \
   -p 8080:4000 \
