89e2d8edf746ad457316e9484471d8b6fdb7893e
Drew Anderson <drew@onlycentral.com>
2020-11-28 11:04:48 +0100

    use podman

--- serve.bash
+++ serve.bash
@@ -7,7 +7,7 @@ fi
 
 echo "Browse to http://127.0.0.1:8080/ to see the served website"
 
-docker run --rm -it \
+podman run --rm -it \
   --volume="$PWD:/srv/jekyll" \
   -p 8080:4000 \
   jekyll/jekyll:4.0 \
