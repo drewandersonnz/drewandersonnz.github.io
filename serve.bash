dac09cf3ffe59ade1625e07af8234c51a2b67faa
Drew Anderson <drewandersonnz@users.noreply.github.com>
2021-05-16 13:46:29 +0200

    update to 4.2

--- serve.bash
+++ serve.bash
@@ -5,7 +5,7 @@ if [ ! -f ./.git/config ]; then
     exit 1;
 fi
 
-export JEKYLL_IMAGE="docker.io/jekyll/jekyll:4.0"
+export JEKYLL_IMAGE="docker.io/jekyll/jekyll:4.2.0"
 
 podman pull ${JEKYLL_IMAGE}
 mkdir .jekyll-cache
